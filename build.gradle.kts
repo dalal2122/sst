e37be4ba29821cbe3ff33f3507bdf178b222f746
Malak Skheita <malak.skheita@s.edu.magwien.gv.at>
2024-02-29 16:14:09 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.3.0-SNAPSHOT"
+	id("io.spring.dependency-management") version "1.1.4"
+}
+
+group = "at.spengergasse"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_17
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+	maven { url = uri("https://repo.spring.io/milestone") }
+	maven { url = uri("https://repo.spring.io/snapshot") }
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	compileOnly("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.security:spring-security-test")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
